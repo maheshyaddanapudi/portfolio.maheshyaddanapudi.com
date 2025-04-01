bcf89fed25b40954abc8b3f682b5114c9daac4e7
maheshyaddanapudi <maheshyaddanapudi@users.noreply.github.com>
2025-04-01 21:01:14 +0000

    deploy: maheshyaddanapudi/maheshyaddanapudi.com@8b5cbaa88a339076166c3b7b77bf25c96ad218ab

---
+++ _next/static/b3jtHMp4g_mQPtfWZ788X/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d92f9472b0f6227e.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
